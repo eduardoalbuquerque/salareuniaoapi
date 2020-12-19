7aa190e5f075dfcfed83bffc825ed3c1b3836ca3
Eduardo Albuquerque <eduardo.albuquerque@hotmail.com.br>
2020-12-19 09:30:01 -0300

    created repository package and interface extends Jpa

---
+++ src/main/java/br/com/solucitiva/saladereuniao/domain/repository/RoomRepository.java
@@ -0,0 +1,8 @@
+package br.com.solucitiva.saladereuniao.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository {
+
+
+}
